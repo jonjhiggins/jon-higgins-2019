5a98609150ede0ee68eb85f7ce59aaf8716576a2
Jon Higgins <jon@jonhiggins.co.uk>
2019-01-28 21:05:13 +1100

    home animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PageWrapper from '~/src/components/page-wrapper'
 import SEO from '~/src/components/seo'
+import HomeAnimation from '~/src/components/home-animation'
 
 const ThisPage = () => {
   return (
     <PageWrapper>
-      <SEO title="Home"/>
-      Home
+      <SEO title="Home" />
+      <HomeAnimation />
     </PageWrapper>
   )
 }
