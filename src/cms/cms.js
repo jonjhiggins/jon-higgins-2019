503796d8387f4695cdf31a4d46afaaed801b158e
Jon Higgins <jon@jonhiggins.co.uk>
2019-09-10 16:13:55 +0100

    remove console.log

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,15 +6,11 @@
 import CMS, { init } from 'netlify-cms-app'
 import * as FileSystemBackend from 'netlify-cms-backend-fs'
 
-console.log(FileSystemBackend)
-
 // If running in development
 if (process.env.NODE_ENV === 'development') {
   window.CMS_ENV = 'development_overrides' // Set the CMS_ENV to the development_ overrides.
   CMS.registerBackend('file-system', FileSystemBackend) // Register the FileSystemBackend.
 }
-
-
 
 // Start NetlifyCMS
 init()
